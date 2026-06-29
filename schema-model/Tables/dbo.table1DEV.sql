9517bf80c3e1b00f5946cf6cec4e48a3fa9203af
pe-dhruv <dhruv.ghori@pe.solutions>
2026-06-29 20:50:14 +0530

    datatypechangetry2

--- schema-model/Tables/dbo.table1DEV.sql
+++ schema-model/Tables/dbo.table1DEV.sql
@@ -3,6 +3,7 @@ CREATE TABLE [dbo].[table1DEV]
 [column1] [nvarchar] (50) NULL,
 [column2] [int] NULL,
 [column3] [bigint] NULL,
-[datatypechange] [varchar] (256) NULL
+[datatypechange] [varchar] (256) NULL,
+[datatypechangetry2] [varchar] (128) NULL
 )
 GO
