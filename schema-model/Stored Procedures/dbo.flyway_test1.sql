17986b3ff5083c2e4387e41f05f82b3bc5f0bd49
pe-dhruv <dhruv.ghori@pe.solutions>
2026-06-30 13:25:13 +0530

    updated

--- schema-model/Stored Procedures/dbo.flyway_test1.sql
+++ schema-model/Stored Procedures/dbo.flyway_test1.sql
@@ -2,9 +2,11 @@ SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
-create proc [dbo].[flyway_test1]
+create   proc [dbo].[flyway_test1]
 as
 begin
 select * FROM SYS.TABLES
+WHERE NAME LIKE '%TEMP%'
+and name not like '%temp%'
 END
 GO
